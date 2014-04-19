416335b7cd35acb92117c28b6ea29813d6213012
rpcao <rpcao@andrew.cmu.edu>
2014-04-19 13:48:41 -0400

    Moved ZxingAdapter to a non-Android project in libraries.
    ZxingAdapter now assumes that the image is reflected for Kinect.

---
+++ libraries/ZxingAdapter/src/ZxingAdapterTest/ZxingAdapterTest.java
@@ -0,0 +1,13 @@
+package ZxingAdapterTest;
+
+public class ZxingAdapterTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
